0aeea0281eae333565ae4cfa2a247751acdf9af8
tonywzwz <161043062+tonywzwz@users.noreply.github.com>
2024-10-03 12:16:34 +0000

    Pronto

--- project/main.py
+++ project/main.py
@@ -21,77 +21,53 @@ def limpar():
     system("cls||clear")
 
 def cliente_valido():
-
     limpar()
-
     endereco_cliente = Endereco("Rua Boa Esperança de Ilha Amarela", "130", "1º Andar", "40711 - 600", "Salvador", UnidadeFederativa.BAHIA)
-
-    cliente = Cliente(123,"Douglas", "71 98472 - 9798", "dougxandy@gmail.com", endereco_cliente, Sexo.MASCULINO, EstadoCivil.SOLTEIRO, "25/03/2007", "123_PA")
-
+    cliente = Cliente(123, "Anthony", "71 98472 - 9798", "anthony@gmail.com", endereco_cliente, Sexo.MASCULINO, EstadoCivil.SOLTEIRO, "25/03/2007", "123_PA")
     print(cliente)
 
 def ps_valido():
-
-    """ps = Prestacao Servico"""
-
     limpar()
-    
     endereco_ps = Endereco("Avenida Paulista", "1578", "9º Andar", "01310-200", "São Paulo", UnidadeFederativa.SAO_PAULO)
-
-    ps = PrestacaoServico(321, "Átila", "4002 - 8922", "atila123@gamil.com", endereco_ps, "12.123.123/000", "SEFAZ", "25/03/2007", "Pendente...")
-
+    ps = PrestacaoServico(321, "Bruno", "4002 - 8922", "bruno123@gmail.com", endereco_ps, "12.123.123/000", "SEFAZ", "25/03/2007", "Pendente...")
     print(ps)
 
 def fornecedor_valido():
-
     limpar()
-    
     endereco_fornecedor = Endereco("Avenida Atlântica", "2000", "5º Andar", "22021-001", "Rio de Janeiro", UnidadeFederativa.RIO_DE_JANEIRO)
-
     fornecedor = Fornecedor(213, "João", "8922 - 4002", "joao123@gmail.com", endereco_fornecedor, "12.345.678/000", "SEFAZ", "Caixas")
-
-    print(fornecedor) 
+    print(fornecedor)
 
 def advogado_valido():
-    
     limpar()
-
     endereco_advogado = Endereco("Rua Augusta", "1508", "3º Andar", "01304-001", "São Paulo", UnidadeFederativa.SAO_PAULO)
-
-    advogado = Advogado(312,"Carlos", "71 98842 - 6370", "carlos@gmail.com", endereco_advogado, Sexo.MASCULINO, EstadoCivil.SOLTEIRO, "23/03/1984", "025.887.035.45", "12.345.678-9", "321123_M", Setor.JURIDICO, 3200.00, "3309_OAB")
-
+    advogado = Advogado(312, "Carlos", "71 98842 - 6370", "carlos@gmail.com", endereco_advogado, Sexo.MASCULINO, EstadoCivil.SOLTEIRO, "23/03/1984", "025.887.035.45", "12.345.678-9", "321123_M", Setor.JURIDICO, 3200.00, "3309_OAB")
     print(advogado)
 
 def medico_valido():
-
     limpar()
-
     endereco_medico = Endereco("Avenida Brasil", "500", "10º Andar", "22290-140", "Rio de Janeiro", UnidadeFederativa.RIO_DE_JANEIRO)
-
     medico = Medico(
         456, 
         "Dr. João Silva", 
         "21 99999-1234", 
-    "dr.joao.silva@gmail.com", 
-    endereco_medico, 
-    Sexo.MASCULINO, 
-    EstadoCivil.CASADO, 
-    "15/08/1975", 
-    "123.456.789-00", 
-    "98.765.432-1",
-    "123222_M",
-    Setor.SAUDE,
-    12000.00, 
-    "CRM_67890"
+        "dr.joao.silva@gmail.com", 
+        endereco_medico, 
+        Sexo.MASCULINO, 
+        EstadoCivil.CASADO, 
+        "15/08/1975", 
+        "123.456.789-00", 
+        "98.765.432-1",
+        "123222_M",
+        Setor.SAUDE,
+        12000.00, 
+        "CRM_67890"
     )
     print(medico)
 
 def engenheiro_valido():
-
     limpar()
-    
     endereco_engenheiro = Endereco("Avenida Paulista", "1000", "7º Andar", "01310-100", "São Paulo", UnidadeFederativa.SAO_PAULO)
-
     engenheiro = Engenheiro(
         789, 
         "Eng. Marcos Andrade", 
@@ -107,17 +83,13 @@ def engenheiro_valido():
         Setor.ENGENHARIA,
         15000.00, 
         "CREA_12345"
-        )
-
+    )
     print(engenheiro)
 
-"""Funções de Instanciamento das classes para Testes:"""
-
-"""
-cliente_valido()
-ps_valido()
-fornecedor_valido()
-advogado_valido()
-medico_valido()
-engenheiro_valido()
-"""
+# Chamar as funções para teste
+# cliente_valido()
+# ps_valido()
+# fornecedor_valido()
+# advogado_valido()
+# medico_valido()
+# engenheiro_valido()
